3ec8c6ae9e12ed4bb06fbb1392812f49b7580b30
Maxwell Mapako <wax911@users.noreply.github.com>
2026-05-29 20:45:14 +0200

    fix: suppress Dokka 'main' source set to prevent overlapping source root validation error (#452)
    Dokka V2 creates both 'main' (internally remapped to 'androidJvm') and
    'release' source sets for Android library modules, both pointing to
    src/main/kotlin. Suppressing the 'main' source set eliminates the
    pre-generation validity check failure:
    'Source sets androidJvm and release have the common source roots'
    
    Fixes the failing CI at:
    https://github.com/AniTrend/support-markdown/actions/runs/26652957865

--- buildSrc/src/main/java/co/anitrend/support/markdown/buildSrc/plugin/components/AndroidPlugins.kt
+++ buildSrc/src/main/java/co/anitrend/support/markdown/buildSrc/plugin/components/AndroidPlugins.kt
@@ -3,6 +3,7 @@ package co.anitrend.support.markdown.buildSrc.plugin.components
 import co.anitrend.support.markdown.buildSrc.common.isLibraryModule
 import co.anitrend.support.markdown.buildSrc.common.isSampleModule
 import org.gradle.api.Project
+import org.jetbrains.dokka.gradle.DokkaExtension
 
 private fun Project.applyModulePlugin() {
     plugins.apply("com.diffplug.spotless")
@@ -10,9 +11,24 @@ private fun Project.applyModulePlugin() {
         plugins.apply("com.android.library")
         plugins.apply("org.jetbrains.dokka")
         plugins.apply("maven-publish")
+        configureDokka()
     }
     else
         plugins.apply("com.android.application")
+}
+
+private fun Project.configureDokka() {
+    plugins.withId("org.jetbrains.dokka") {
+        afterEvaluate {
+            extensions.configure(DokkaExtension::class.java) {
+                dokkaSourceSets.all {
+                    if (name == "main") {
+                        suppress.set(true)
+                    }
+                }
+            }
+        }
+    }
 }
 
 internal fun Project.configurePlugins() {
